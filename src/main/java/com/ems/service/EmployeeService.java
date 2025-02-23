ff3864ee98f4968d742a21062f3032ac4a63bf44
Kumari-Reena <Kumari.Reena@Chubb.com>
2025-02-23 15:43:12 +0530

    initial commit

---
+++ src/main/java/com/ems/service/EmployeeService.java
@@ -0,0 +1,41 @@
+package com.ems.service;
+
+import com.ems.model.Employee;
+import com.ems.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EmployeeService {
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    public Employee getEmployeeById(Long id) {
+        return employeeRepository.findById(id).orElse(null);
+    }
+
+    public Employee createEmployee(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    public Employee updateEmployee(Long id, Employee employeeDetails) {
+        Employee employee = employeeRepository.findById(id).orElse(null);
+        if (employee != null) {
+            employee.setName(employeeDetails.getName());
+            employee.setDepartment(employeeDetails.getDepartment());
+            employee.setRole(employeeDetails.getRole());
+            return employeeRepository.save(employee);
+        }
+        return null;
+    }
+
+    public void deleteEmployee(Long id) {
+        employeeRepository.deleteById(id);
+    }
+}
