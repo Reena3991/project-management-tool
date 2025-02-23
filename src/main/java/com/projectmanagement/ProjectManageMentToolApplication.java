9e6385a254001c15a22ec424b09885af882b5755
Kumari-Reena <Kumari.Reena@Chubb.com>
2025-02-23 15:55:57 +0530

    initial

---
+++ src/main/java/com/projectmanagement/ProjectManageMentToolApplication.java
@@ -0,0 +1,12 @@
+package com.projectmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectManageMentToolApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectManageMentToolApplication.class, args);
+    }
+
+}
